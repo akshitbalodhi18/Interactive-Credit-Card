fad551d47df9d9a11f4466bbb6912cf93cee16d0
akshitbalodhi18 <akshitbalodhi18@gmail.com>
2024-02-17 06:09:57 +0530

    Added Card front details, Card back details, added Confirmation Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import "./App.css";
+import CardBack from "./Components/CardBack";
+import CardFront from "./Components/CardFront";
 import CardForm from "./Components/CardForm";
+import { useState } from "react";
+import CompletePage from "./Components/CompletePage";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    number: "",
+    month: "",
+    year: "",
+    cvc: "",
+  });
+
+  const [validSubmit, setValidSubmit] = useState(false);
+
   return (
-    <>
-      <CardForm />
-    </>
+    <div className="App">
+      <div className="flex">
+        <div className="design">
+          <CardBack cvc={formData.cvc} />
+          <CardFront {...formData} />
+        </div>
+        <div className="container">
+          {!validSubmit ? (
+            <CardForm
+              {...formData}
+              setFormData={setFormData}
+              setValidSubmit={setValidSubmit}
+            />
+          ) : (
+            <CompletePage
+              setFormData={setFormData}
+              setValidSubmit={setValidSubmit}
+            />
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
